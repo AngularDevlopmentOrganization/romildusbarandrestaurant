4b24e4fe80868d0bece931529bc95fcafad4c722
dlbmc <dlbmc@dlbmc.com>
2015-09-30 08:31:27 -0300

    Configuração do indicador de loading

--- romildusbarandrestaurantapp/www/js/controllers.js
+++ romildusbarandrestaurantapp/www/js/controllers.js
@@ -38,7 +38,7 @@ angular.module('openDataApplication')
 
    function carregarCategorias($scope,restService,$timeout,ionicMaterialInk,ionicMaterialMotion,$ionicLoading){
      var categories = restService.obterCategorias();
-     categories.then(function(response){       
+     categories.then(function(response){
        $scope.categorias = response;
        $scope.loadingIndicator = $ionicLoading.hide();
        aplicarEfeitoBlinds($timeout, ionicMaterialInk, ionicMaterialMotion, 200);
@@ -48,11 +48,27 @@ angular.module('openDataApplication')
    function carregarBarERes($scope,restService,$timeout,ionicMaterialInk,ionicMaterialMotion,$ionicLoading){
      var barERelDataModel = restService.obterBareRes();
      barERelDataModel.then(function(response){
-       $scope.places = response.results;
+       $scope.places = [];
+       carregarLugaresComVelocidade($scope, $timeout, response, 300);
        $scope.dataModel = barERelDataModel;
        $scope.loadingIndicator = $ionicLoading.hide();
        aplicarEfeitoBlinds($timeout, ionicMaterialInk, ionicMaterialMotion, 200);
      })
+   }
+
+   function carregarLugaresComVelocidade($scope, $timeout, response, velocidade){
+     var place = 0;
+     carregarLugares($scope, $timeout, response, place, velocidade);
+   }
+
+   function carregarLugares($scope, $timeout, response, place, velocidade){
+     $timeout(function () {
+       $scope.places[place] = response.results[place];
+        place++;
+       if(place < response.results.length){
+         carregarLugares($scope, $timeout, response, place, velocidade);
+       }
+     }, velocidade);
    }
 
    function aplicarEfeitoBlinds($timeout,ionicMaterialInk,ionicMaterialMotion, tempoDeEspera){
