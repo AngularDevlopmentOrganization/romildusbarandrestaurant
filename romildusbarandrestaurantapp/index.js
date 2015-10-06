228ab7aca0722efd2cb0c3554dbcbaf0c0e45337
dlbmc <dlbmc@dlbmc.com>
2015-10-06 13:31:19 -0300

    JSON Data

---
+++ romildusbarandrestaurantapp/index.js
@@ -0,0 +1,23 @@
+var contents = require("./hoteis.json");
+
+console.log(contents);
+
+// var fs = require('fs');
+//
+// function loadJSONFile(filename, encoding){
+//
+//   try{
+//
+//     if(typeof(encoding) == 'undefined'){
+//       encoding = 'utf8';
+//     }
+//
+//     var contents = fs.readFileSync(fileName, encoding);
+//
+//     return JSON.parse(contents);
+//
+//   }catch(err){
+//     throw err;
+//   }
+//
+// }
